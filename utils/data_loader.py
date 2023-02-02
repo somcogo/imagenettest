ae33f290a68c2c463f989f87ff510ce5c85b86bf
Soma <somahansel@gmail.com>
2023-02-02 17:12:06 +0100

    preprocessing and dataloader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,42 +1,45 @@
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Normalize, Compose
 from torch.utils.data import DataLoader, Dataset
+import h5py
+import os
+import numpy as np
+import torch
 
-data_path = '../data/'
-mean = 0.1307
-mean_val = 0.1325
-std = 0.3081
-std_val = 0.3105
+data_path = 'data'
 
-trn_datalist = ''
-val_datalist = ''
-
-trn_transforms = Compose([
-    ToTensor(),
-    Normalize(mean, std)
-])
-val_transforms = Compose([
-    ToTensor(),
-    Normalize(mean, std)
-])
-
-class SampleDataset(Dataset):
-    def __init__(self, datalist):
+class DataSet(Dataset):
+    def __init__(self, data_path, mode):
         super().__init__()
-        self.datalist = datalist
+        h5_file = h5py.File(os.path.join(data_path, 'tiny_imagenet_{}.hdf5'.format(mode)), 'r')
+        self.image_ds = h5_file['data']
+        if h5_file['labels'] is not None:
+            labels_ds = h5_file['labels']
+            self.labels = labels_ds.asstr()[()]
     
     def __len__(self):
-        return len(self.datalist)
+        return self.image_ds.shape[0]
 
     def __getitem__(self, index):
-        return self.datalist[index]
+        image_np = np.array(self.image_ds[index])
+        image = torch.from_numpy(image_np)
+        if self.labels is not None:
+            return image, self.labels
+        else:
+            return image
         
-trn_dataset = SampleDataset(data=trn_datalist)
-val_dataset = SampleDataset(data=val_datalist)
-    
+trn_dataset = DataSet(data_path=data_path, mode='trn')
+val_dataset = DataSet(data_path=data_path, mode='val')
+tst_dataset = DataSet(data_path=data_path, mode='tst')
 
-def get_loader(batch_size):
-    train_loader = DataLoader(trn_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+def get_trn_loader(batch_size):
+    train_loader = DataLoader(trn_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+    return train_loader
 
-    return train_loader, val_loader
+def get_val_loader(batch_size):
+    train_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, drop_last=False)
+    return train_loader
+
+def get_tst_loader(batch_size):
+    train_loader = DataLoader(tst_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+    return train_loader
